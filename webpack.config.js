1824356d5446ee3dfaa745cbdca20ed0435476c0
scars <scars377@gmail.com>
2018-09-05 13:25:48 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  // mode: 'development',
+  devtool: 'eval',
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/data': 'http://localhost:8081',
+    },
+  },
+
+  // context: path.resolve('./src'),
+  entry: {
+    main: path.resolve('./src/main.js'),
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].[hash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve('./src'),
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
